de2f0a5f0c666d326846edf1123ba15a6f2de765
mmcentre <michael.bouzinier@kvitneset.com>
2021-12-16 22:39:42 -0500

    MAB Updating copyright and license

--- src/python/epa/aqs.py
+++ src/python/epa/aqs.py
@@ -11,6 +11,25 @@ https://aqs.epa.gov/aqsweb/airdata/download_files.html
 The tool also adds a column containing a uniquely generated Monitor Key
 
 """
+#  Copyright (c) 2021. Harvard University
+#
+#  Developed by Research Software Engineering,
+#  Faculty of Arts and Sciences, Research Computing (FAS RC)
+#  Author: Michael A Bouzinier
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
 import os
 
 from epa.aqs_ds_def import AQSContext
